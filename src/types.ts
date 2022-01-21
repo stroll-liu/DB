9ad76903cd8ffe612525790818de85572912cb58
liuyi <13403111884@163.com>
2022-01-22 01:10:17 +0800

    -

---
+++ src/types.ts
@@ -0,0 +1,57 @@
+import DB from '../index';
+import DBTable from './table';
+
+type PrimitiveType = number | string | boolean | null;
+type DBDataType = PrimitiveType | object;
+
+export type DBClass = DB;
+export type DBTableClass = DBTable;
+
+export interface DBData {
+  id: number,
+  [key: string]: DBDataType | Array<DBDataType>
+}
+
+export interface DBInputData {
+  [key: string]: DBDataType | Array<DBDataType>
+}
+
+export interface DBTableSearch {
+  [key: string]: number | string
+}
+
+export type GetDBCallback = (idb: IDBDatabase) => void;
+
+export type TableFindFunction = (item?: DBData) => boolean;
+
+export type TableIndexTypes =
+  NumberConstructor
+  | StringConstructor
+  | BooleanConstructor
+  | DateConstructor
+  | ObjectConstructor
+  | ArrayConstructor;
+
+export interface TableIndex {
+  type: TableIndexTypes
+  multiEntry?: boolean
+  unique?: boolean
+  default?: any
+  ref?: string
+}
+
+export interface DBConfig {
+  version?: number
+}
+
+export interface DBTableSchema {
+  [key: string]: TableIndex | TableIndexTypes
+}
+
+export interface DBSchema {
+  [table: string]: DBTableSchema
+}
+
+export interface DBTableDict {
+  [table: string]: DBTableClass
+}
